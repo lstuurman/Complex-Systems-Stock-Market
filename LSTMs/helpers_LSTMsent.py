7510c8eab239b4bd9cce2605dae4da38d71f767a
Laurens Stuurman <laurenssturman678@hotmail.com>
2020-02-23 20:51:11 +0100

    bug

--- LSTMs/helpers_LSTMsent.py
+++ LSTMs/helpers_LSTMsent.py
@@ -83,7 +83,7 @@ def get_minibatch2(data, batch_size=25, shuffle=True):
   data now is a pandas dataframe
   """
   # convet pandas df to list of tuples : 
-  data = [(tknzr.tokenize(x[1]),x[0]) for x in data.to_list()]
+  data = [(tknzr.tokenize(x[1]),x[0]) for x in data.values.to_list()]
 
   if shuffle:
     print("Shuffling training data")
